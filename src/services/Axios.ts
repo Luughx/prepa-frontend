21be3f7d16045c6b81b1f385e55b7eaef3e9d7aa
Lughx <lughxdr@gmail.com>
2023-01-29 18:37:14 -0600

    test 6

--- src/services/Axios.ts
+++ src/services/Axios.ts
@@ -5,8 +5,9 @@ const axiosIntance: AxiosInstance = axios.create({
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Credentials': 'true'
         //'Content-Type': 'multipart/form-data',
     }
 })
