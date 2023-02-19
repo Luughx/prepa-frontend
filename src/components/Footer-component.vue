0b27b8d0d9981009dffdfe01e39ca48a3d185aa4
Lughx <lughxdr@gmail.com>
2023-02-19 09:03:36 -0600

    seo 2

--- src/components/Footer-component.vue
+++ src/components/Footer-component.vue
@@ -7,9 +7,21 @@
                     <span class="mb-3 mb-md-0 text-muted">&copy; 2023 Escuela Preparatoria Activo 20-30 Albert Einstein</span>
                 </div>
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <li class=""><a target="_blank" class="text-secondary" href="https://www.facebook.com/preparatoria2030/"><font-awesome-icon class="icon" icon="fa-brands fa-facebook" /></a></li>
-                    <li class="ms-3"><a target="_blank" class="text-muted" href="https://www.instagram.com/prepa2030alberteinstein/"><font-awesome-icon class="icon" icon="fa-brands fa-instagram" /></a></li>
-                    <li class="ms-3"><a target="_blank" class="text-muted" href="https://www.youtube.com/channel/UC87kIuNL2TuL_4EXOHYud7w"><font-awesome-icon class="icon-yt" icon="fa-brands fa-youtube" /></a></li>
+                    <li>
+                        <a target="_blank" class="text-secondary" href="https://www.facebook.com/preparatoria2030/" aria-label="boton facebook">
+                            <font-awesome-icon class="icon" icon="fa-brands fa-facebook" />
+                        </a>
+                    </li>
+                    <li class="ms-3">
+                        <a target="_blank" class="text-muted" href="https://www.instagram.com/prepa2030alberteinstein/" aria-label="boton instagram">
+                            <font-awesome-icon class="icon" icon="fa-brands fa-instagram" />
+                        </a>
+                    </li>
+                    <li class="ms-3">
+                        <a target="_blank" class="text-muted" href="https://www.youtube.com/channel/UC87kIuNL2TuL_4EXOHYud7w" aria-label="boton youtube">
+                            <font-awesome-icon class="icon-yt" icon="fa-brands fa-youtube" />
+                        </a>
+                    </li>
                 </ul>
             </footer>
         </div>
