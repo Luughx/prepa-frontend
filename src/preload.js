45dc3ad842c0337d49a7d35147a0b6a7d7b2c185
Lughx <lughxdr@gmail.com>
2023-02-20 21:02:11 -0600

    night theme in load

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
     const element = document.getElementById("loadingMain");
+    document.body.classList.remove("night-loader")
     element.style.display = "none"
 })
