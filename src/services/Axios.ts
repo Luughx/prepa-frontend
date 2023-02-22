1f339442de3f572b7f527c7ba3a0378648b16022
Lughx <lughxdr@gmail.com>
2023-02-21 20:16:57 -0600

    fix destination axios

--- src/services/Axios.ts
+++ src/services/Axios.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosInstance } from "axios"
 
 const axiosIntance: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: process.env.NODE_ENV == "production" ? "https://server.daraan.site/api" : "http://localhost:3000/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
-        //'Content-Type': 'multipart/form-data',
         "Access-Control-Allow-Origin": "*"
     }
 })
