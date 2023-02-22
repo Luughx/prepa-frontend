cee88150405c463a90cbfc7b522306a716d20d57
Lughx <lughxdr@gmail.com>
2023-02-22 15:17:59 -0600

    prenrendering test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const plugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  
+  const PrerendererWebpackPlugin = require('@prerenderer/webpack-plugin')
+  const { join } = require('path')
+  
+	plugins.unshift(
+		new PrerendererWebpackPlugin({
+			routes: ["/", "/avisos"]
+		})
+	)
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins
+  }
 })
